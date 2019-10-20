b34c4dd4025a5a62f02394bf6152cf17eafdcad0
Yiheng (NUC) <development.henryquan@gmail.com>
2019-10-20 16:28:22 +1100

    update guard to work anywhere

--- guard.js
+++ guard.js
@@ -10,49 +10,42 @@ const mydata = {
         no: '6',
         hello: {
           yes: '7',
-          no: '8',
-        },
-      },
-    },
+          no: '8'
+        }
+      }
+    }
   },
   list: [
-    {1: { num: 1 }},
-    {2: { num: 2 }},
-    {3: { num: 3 }},
-    {4: { num: 4 }},
-    {5: { num: 5 }},
+    { 1: { num: 1 } },
+    { 2: { num: 2 } },
+    { 3: { num: 3 } },
+    { 4: { num: 4 } },
+    { 5: { num: 5 } },
   ]
-}
+};
 
 /**
  * Reference: https://medium.com/javascript-inside/safely-accessing-deeply-nested-values-in-javascript-99bf72a0855a
  */
-
 // take object, path and the default value
-const guard = (path, dval) => {
+var guard = function (obj, path, dval) {
+  if (obj == null) return dval;
   // check if object is valid and path does not start with or end with '.'
   if (!path.startsWith('.') && !path.endsWith('.')) {
     // get path as an array and it must have at least 2 elements
-    let p = path.split('.');
+    var p = path.split('.');
     if (p && p.length > 0) {
-      // eval the first string to be object
-      let obj = eval(p.shift());
-
       // o is the object (accumulator), and n is from path (current value)
       // o && o[n] -> to go further or just return default value
       // only asking for the object
-      if (p.length == 1) return obj;
-      return p.reduce((o, n) => (o && o[n]) ? o[n] : dval, obj);
+      return p.reduce(function (o, n) { return (o && o[n]) ? o[n] : dval; }, obj);
     }
-  }   
+  }
   return dval;
-}
+};
 
 // Some basic tests
 var assert = require('assert');
-assert(guard('mydata.hello.hello.hello.hello.yes', 0) == 7);
-assert(guard('mydata', 0) === mydata);
-assert(guard('mydata.hello.hello.hello.hello.hello', false) == false);
-assert(guard('mydata.hello.hello.dsadas', 0) == 0);
-
-// If you pass undefined obj eval will complain
+assert(guard(mydata, 'hello.hello.hello.hello.yes', 0) == 7);
+assert(guard(mydata, 'hello.hello.hello.hello.hello', false) == false);
+assert(guard(mydata, 'hello.hello.dsadas', 0) == 0);
